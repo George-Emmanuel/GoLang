0004c56add48a74dac7b8b3a8a68c1cc3a3f7ccf
George Emmanuel <george444emmanuel@gmail.com>
2025-06-18 02:53:48 +0530

    MP3 Downloader

---
+++ Go/MP3.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	var url string
+	fmt.Print("Enter YouTube URL: ")
+	fmt.Scanln(&url)
+
+	cmd := exec.Command("yt-dlp", "-x", "--audio-format", "mp3", url)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error downloading:", err)
+	} else {
+		fmt.Println("Download complete.")
+	}
+}
